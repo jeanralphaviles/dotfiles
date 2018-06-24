7099bd6b49bdfe6a136a8a030ac98a3a651756fd
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2018-06-24 10:23:40 -0700

    power_management: Start xscreensaver daemon on startup.

--- i3/scripts/power_management.sh
+++ i3/scripts/power_management.sh
@@ -2,5 +2,6 @@
 
 # Arg[1] is display shutoff time in minutes
 
+xscreensaver &
 xautolock -time "$1" -locker "$HOME/.i3/scripts/lock.sh" &
 python "$HOME/.i3/scripts/disable-standby-fs.py" $[$1 * 60 - 1] &
